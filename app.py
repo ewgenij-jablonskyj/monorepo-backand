a7ed64cb04b3d4b3dc5bc847d8338f28c9243c04
ewgenij.jablonskyj <ewgenij.jablonskyj@gmail.com>
2026-06-17 14:28:13 +0200

    feat: :art: graphql

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask
+import graphene
+from flask_graphql import GraphQLView
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(name=graphene.String(default_value="default"))
+
+    def resolve_hello(self, info, name):
+        return f"Witaj, {name}!"
+
+schema = graphene.Schema(query=Query)
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return {"message": "Hello from your new backend monorepo!"}
+app.add_url_rule(
+    '/',
+    view_func=GraphQLView.as_view(
+        'graphql',
+        schema=schema,
+        graphiql=True
+    )
+)
 
 if __name__ == '__main__':
-    # Run the application in debug mode
     app.run(debug=True, host='127.0.0.1', port=5001)
